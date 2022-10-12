f65e85cb78ffd0a446b2625cc1406ae63b8cb502
Tasneem205 <72310162+Tasneem205@users.noreply.github.com>
2022-10-12 17:18:49 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+import Express from "express";
+import router from "./src/controllers/index.controller.js";
+import dotenv from "dotenv";
+import errorHandler from "./src/middleWares/errorHandler.js";
+import morgan from "morgan";
+
+const app = Express();
+
+dotenv.config();
+
+app.use(Express.json());
+
+app.use(morgan("dev"));
+
+app.use("/hotel", router);
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port 4000");
+});
